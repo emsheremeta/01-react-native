35336a7f0ad90d3c838294abab2d482172097416
Marina <sheremetamaryna@gmail.com>
2023-03-27 10:15:43 +0200

    upd test

--- react-native-project/App.js
+++ react-native-project/App.js
@@ -2,7 +2,8 @@
 
 import React from 'react';
 import { View } from 'react-native';
-import LoginScreen from './Screens/LoginScreen';
+// import LoginScreen from './Screens/LoginScreen';
+import RegistrationScreen from './Screens/RegistrationScreen';
 
 export default function App () {
   return (
